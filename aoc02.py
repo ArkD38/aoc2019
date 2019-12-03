04544431b478767caf5e7119a7c0de0e7da2c231
ArkD38 <57869336+ArkD38@users.noreply.github.com>
2019-12-03 19:25:55 +0100

    Add files via upload

---
+++ aoc02.py
@@ -0,0 +1,33 @@
+print("hello world, this is my solution for Advent of Code 2019 day 2")
+o_lista = [] #Ebbe megy a bemenet
+
+
+#-----()-----
+# Beolvasás
+#-----()-----
+
+with open("in.txt", "r") as f:
+    o_lista = f.read().split(",")
+for i in range(len(o_lista)):
+    o_lista[i] = int(o_lista[i])
+print(o_lista)
+
+
+
+#-----()-----
+# Függvények
+#-----()-----
+
+def darabolo(lista):
+    #lista <- o_lista
+    for i in range(len(lista)):
+        if lista[i] == 1:
+            lista[lista[i+3]] = lista[lista[i+1]] + lista[lista[i+2]]
+            i += 4
+        elif lista[i] == 2:
+            lista[lista[i+3]] = lista[lista[i+1]] * lista[lista[i+2]]
+            i += 4
+        else:
+            break
+        return(lista)
+print(darabolo(o_lista))
